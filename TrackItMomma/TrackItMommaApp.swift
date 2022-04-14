cd49071408108ba4abe97add11f46704aa1bd7ff
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-14 16:46:23 -0700

    Initial Commit

---
+++ TrackItMomma/TrackItMommaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackItMommaApp.swift
+//  TrackItMomma
+//
+//  Created by Andres Gutierrez on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TrackItMommaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
